b334c2ade1e3e567a5714cce6b4043b976e07580
Subodh <subodhlst@gmail.com>
2025-10-29 12:48:50 +0530

    read and set query string

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,21 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Pos</h1>
+      <p>
+        lat: {lat} lng : {lng}
+      </p>
+      <button onClick={() => setSearchParams({ lat: 40, lng: 27 })}>
+        Change pos
+      </button>
+    </div>
+  );
 }
 
 export default Map;
