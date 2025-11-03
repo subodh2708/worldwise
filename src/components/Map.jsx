92a6d31781bd1cc4861738c25393fd5a54106dac
Subodh <subodhlst@gmail.com>
2025-11-03 23:53:26 +0530

    resolve flag visibility issue

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,12 +2,17 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./map.module.css";
 import { MapContainer, Popup, TileLayer, Marker } from "react-leaflet";
 import { useState } from "react";
+import { useCities } from "../contexts/CiitesContext";
+import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
+
+polyfillCountryFlagEmojis();
+
 function Map() {
   // const navigate = useNavigate();
   // const [searchParams, setSearchParams] = useSearchParams();
   // const lat = searchParams.get("lat");
   // const lng = searchParams.get("lng");
-
+  const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
   return (
@@ -15,18 +20,24 @@ function Map() {
       <MapContainer
         center={mapPosition}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         className={styles.map}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
